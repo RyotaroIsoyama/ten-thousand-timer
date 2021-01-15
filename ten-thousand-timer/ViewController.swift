1a47e6f1f1e0f3ae9fb59b735953ad663812754d
RyotaroIsoyama <madao329@gmail.com>
2021-01-16 02:09:12 +0900

    Initial Commit

---
+++ ten-thousand-timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ten-thousand-timer
+//
+//  Created by 磯山椋太郎 on 2021/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
