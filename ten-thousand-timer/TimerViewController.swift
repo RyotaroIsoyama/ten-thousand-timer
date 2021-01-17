c84e4ee30448ed9d34d5158b91dc047f93bd1fc8
RyotaroIsoyama <madao329@gmail.com>
2021-01-17 18:26:46 +0900

    save 1/17

--- ten-thousand-timer/TimerViewController.swift
+++ ten-thousand-timer/TimerViewController.swift
@@ -14,7 +14,12 @@ class TimerViewController: UIViewController {
     @IBOutlet weak var seconds_label: UILabel!
     @IBOutlet weak var sub_seconds_label: UILabel!
     
-    var counting:Bool = false
+    var timer = Timer()
+    var count:float = 0
+    var hours:Int = 0
+    var minutes:Int = 0
+    var seconds:Int = 0
+    var sub_seconds:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,15 +28,22 @@ class TimerViewController: UIViewController {
     }
     
     func fetchTime(){
-        
+        hours = UserDefaults.standard.integer(forKey: "hours")
+        minutes = UserDefaults.standard.integer(forKey: "minutes")
+        seconds = UserDefaults.standard.integer(forKey: "seconds")
+        sub_seconds = UserDefaults.standard.integer(forKey: "sub_seconds")
     }
     
     @IBAction func tapTimer(_ sender: Any) {
-        if counting {
-            //止める処理
-        }else{
+        if !timer.isValid {
             //動かす処理
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: <#T##Selector#>, userInfo: nil, repeats: true)
+        }else{
+            //止める処理
         }
     }
-    
+    @objc func updateTimeLabel(){
+        count += 0.01
+        
+    }
 }
